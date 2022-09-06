1ffed78d50021a76efa3c4e93eb6916bb47c0d54
Thant Ko Zaw <thantmkou10@gmail.com>
2022-09-06 13:18:56 +0630

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
